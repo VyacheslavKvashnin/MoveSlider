71b23f92c27619826be86181f67b7d6b22feff73
komplektan <54449303+komplektan@users.noreply.github.com>
2022-04-23 00:02:51 +0500

    Initial Commit

---
+++ MoveSlider/MoveSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveSliderApp.swift
+//  MoveSlider
+//
+//  Created by Вячеслав Квашнин on 23.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoveSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
