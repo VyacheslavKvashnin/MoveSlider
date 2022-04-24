ce12411988c05f186ddd060cb11aed840587be3b
komplektan <54449303+komplektan@users.noreply.github.com>
2022-04-24 21:11:43 +0500

    Finished project

--- MoveSlider/SliderUIKit.swift
+++ MoveSlider/SliderUIKit.swift
@@ -9,23 +9,48 @@ import SwiftUI
 
 struct SliderUIKit: UIViewRepresentable {
     
-    @Binding var value: Float
+    @Binding var currentValue: Float
+    let alpha: Int
     
     func makeUIView(context: Context) -> UISlider {
         let slider = UISlider()
-        slider.setValue(0.5, animated: true)
-        
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.addNewValue),
+            for: .valueChanged
+        )
         
         return slider
     }
     
     func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.thumbTintColor = UIColor(red: 1, green: 0, blue: 0, alpha: CGFloat(alpha) / 100)
+        uiView.value = currentValue
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(value: $currentValue)
+    }
+}
+
+extension SliderUIKit {
+    class Coordinator: NSObject {
+        @Binding var value: Float
         
+        init(value: Binding<Float>) {
+            self._value = value
+        }
+        
+        @objc func addNewValue(_ sender: UISlider) {
+            value = sender.value
+        }
     }
 }
 
 struct SliderUIKit_Previews: PreviewProvider {
     static var previews: some View {
-        SliderUIKit(value: .constant(2.0))
+        SliderUIKit(currentValue: .constant(2.0), alpha: 2)
     }
 }
